8a7bf3c5ae1fb95cb312962dbf7635413584478f
Rosa-musi <nadamejor27@hotmail.com>
2021-07-19 11:34:40 -0500

    funciones de sort

--- src/data.js
+++ src/data.js
@@ -14,6 +14,26 @@ export const maping = (data) => {
   return posters2
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+
+export const AlphabeticalOrderSort = (data) => {
+  let result = data.films.sort((a, b) => {
+    const titleA = a.title.toLowerCase()
+    const titleB = b.title.toLowerCase()
+    if (titleA < titleB) {
+      return -1;
+    }
+    if (titleA > titleB) {
+      return 1
+    } else{
+      return 0;
+    }
+  })
+  return result
+} 
+
+export const sortData = (data ) => {
+   const sortRelease = data.films.sort((a,b) => (a.release_date > b.release_date ? 1 : -1)); 
+  return sortRelease;
+  
+} 
+  
