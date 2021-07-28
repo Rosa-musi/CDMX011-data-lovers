477a13b88f15ad0dc9ab701b4737f2d0349a9cb0
Rosa-musi <nadamejor27@hotmail.com>
2021-07-28 12:56:45 -0500

    Updates

--- data.js
+++ data.js
@@ -1,10 +1,81 @@
 // estas funciones son de ejemplo
-import data from './data/ghibli/ghibli.js'
-export const ghibliMovies = data.films.map(data => [`${data.title} ${data.poster} ${data.description}`]);
 
-  
-//};
+export const maping = (data) => {
+  let posters2 = []
 
-export const anotherExample = () => {
-  return 'OMG';
+  data.forEach(movie => {
+    posters2 += `
+    <div class="card_film"> 
+      <div class="posterContainer">
+        <img src=${movie.poster} class= "posterimg" alt="${movie.title}">
+      </div>
+      <div class= "movie_info">
+        <div class ="movieDate">
+          <p class="releaseDate"> ${movie.release_date}</p>         
+          <p class = "rate"> Score: <span >${movie.rt_score}</span></p>
+        </div>    
+        <h2>${movie.title}</h2>        
+        <p class="director"> <span class="purpleText"> Director: </span> ${movie.director} </p>
+        <p class="producer"> <span class="purpleText"> Producer: </span> ${movie.producer} </p>
+        <p class="description"> ${movie.description} </p>
+      </div>
+    </div>  
+    `
+
+  })
+
+  return posters2
+};
+
+
+
+
+const sortData = (data) => data.sort((a, b) => a.release_date > b.release_date ? 1 : -1)
+export let sortAscending = (data) => sortData(data);
+export const sortDescending = (data) => sortAscending(data).reverse();
+
+export const sortAphabetic = (data) => data.sort((a,b) => a.title > b.title ? 1 : -1);
+
+const sortRate = (data) => data.sort((a, b) => a.rt_score - b.rt_score);
+export const sortWorst = (data) => sortRate(data);
+
+export const filterData = (data, option) => {
+  const filterDirector = data.filter((movie => movie.director === option));
+  return filterDirector;
+}
+
+
+export const peopleArray = (data) => {
+  let peopleArray = []
+  for (let film of data) {
+      const character = film.people.map(function (people){
+        people.movie = film.title
+        return people
+      })
+    peopleArray= peopleArray.concat(character)
+  }
+  return peopleArray
+}
+
+
+export const charactersCard = (data) => {
+  let characters = []
+
+  data.forEach(movie => {
+      characters += `
+      <div class="card_character">         
+          <div class="photoContainer">
+            <img src=${movie.img} class= "photoimg" alt="${movie.name}">
+          </div>
+          <div class ="characterInfo">
+            <div class= "characterData">                   
+              <p class = "movie"> Movie: <br> <span>${movie.movie}</span></p>
+              <p class="Specie"> Specie <br> <span>${movie.specie}</span></p>         
+            </div>
+            <h2 class="characterName">${movie.name}</h2>   
+        </div>
+      </div>
+      `
+  })
+  return characters
 };
--- main.js
+++ main.js
@@ -1,9 +1,86 @@
-import { ghibliMovies } from './data.js';
+import { maping, sortAscending, sortDescending, sortAphabetic, sortWorst, filterData, peopleArray, charactersCard } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
+let films = document.getElementById('films')
+let data_films = data.films
+let filterSort = data_films
+const movies_button = document.getElementById('movies_button');
+const home = document.getElementById('home')
+const home_button = document.getElementById('home_button')
+const selector = document.getElementById('selector')
+const characters = document.getElementById('characters')
+const characters_button = document.getElementById('characters_button')
+const menuHamburguesa = document.getElementById('menuHamburguesa')
+const menu = document.getElementById('menu')
 
 
-console.log(data);
-console.log(ghibliMovies);
+
+menuHamburguesa.addEventListener('click', () =>{
+    menu.classList.toggle('activated')
+})
+
+
+home_button.addEventListener('click', () => {
+    home.style.display = "flex";
+    films.style.display = "none";
+    selector.style.display = "none";
+    characters.style.display = "none"
+})
+
+
+
+movies_button.addEventListener('click', () => {
+    let moviesMaped = maping(data_films)
+    films.innerHTML = moviesMaped
+    home.style.display = "none";
+    films.style.display = "flex";
+    selector.style.display = "flex";
+    characters.style.display = "none"
+})
+
+
+const sortSelect = document.querySelector('.sortMovies')
+sortSelect.addEventListener('change', (e) => {
+    if (e.target.value === "alphabetic"){
+        let sort = sortAphabetic(filterSort)
+        let printMovies = maping(sort)
+        films.innerHTML = printMovies
+    } else if (e.target.value === "releaseAscending"){
+        let sort = sortAscending(filterSort)
+        let printMovies = maping(sort)
+        films.innerHTML = printMovies
+    } else if (e.target.value === "releaseDescending"){
+        let sort = sortDescending(filterSort)
+        let printMovies = maping(sort)
+        films.innerHTML = printMovies
+    } else if (e.target.value === "score") {
+        let sort = sortWorst(filterSort)
+        let printMovies = maping(sort)
+        films.innerHTML = printMovies
+        
+    }
+})
+
+const filterSelect = document.querySelector('.filterMovies')
+filterSelect.addEventListener('change', (event) => {
+    if(event.target.value != "all"){        
+        filterSort = filterData(data_films, event.target.value)
+        let filtered = maping(filterSort)
+        films.innerHTML = filtered
+    } else {
+        filterSort = data_films
+        films.innerHTML = maping(filterSort)
+    }
+})
+
+characters_button.addEventListener('click', () => {
+    home.style.display = "none";
+    films.style.display = "none";
+    selector.style.display = "none";
+    characters.style.display = "flex"
+    let arrayPeople = peopleArray(data_films)
+    let charactersPrint = charactersCard(arrayPeople)
+    characters.innerHTML = charactersPrint
+})
